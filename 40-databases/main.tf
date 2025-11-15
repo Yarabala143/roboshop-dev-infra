8c2125b787cdee4f8efeeabe9d07c8f3f08ea457
unknown <Satish.yy@gmail.com>
2025-11-15 19:43:52 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mongodb" {
     ami = local.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.mongodb_sg_id]
-    subnet_id = local.database_subnet_ids
+    subnet_id = local.database_subnet_id
        
     tags = merge (
         local.common_tags,
@@ -74,6 +74,92 @@ resource "terraform_data" "redis" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
         "sudo sh /tmp/bootstrap.sh redis"
+    ]
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.rabbitmq_sg_id]
+    subnet_id = local.database_subnet_id
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-rabbitmq" # roboshop-dev-rabbitmq
+        }
+    )
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh rabbitmq"
+    ]
+  }
+}
+
+resource "aws_instance" "mysql" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.mysql_sg_id]
+    subnet_id = local.database_subnet_id
+    iam_instance_profile = aws_iam_instance_profile.mysql.name
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-mysql" # roboshop-dev-mysql
+        }
+    )
+}
+
+resource "aws_iam_instance_profile" "mysql" {
+  name = "mysql"
+  role = "EC2SSMParameterRead"
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
+
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql dev"
     ]
   }
 }
