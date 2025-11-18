6df1a96977cc3fe3cecd493e4feb5bf6cf026b18
unknown <Satish.yy@gmail.com>
2025-11-18 23:12:51 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -34,7 +34,7 @@ resource "terraform_data" "catalogue" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/catalogue.sh",
-        "sudo sh /tmp/catalogue.sh catalogue"
+        "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
         ]
   }
 }
