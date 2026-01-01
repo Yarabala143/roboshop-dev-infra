dfb0e2756ee525683e676b5a4667442f1ac708c8
unknown <Satish.yy@gmail.com>
2026-01-01 12:42:06 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "yarabala.space"
+}
+
+variable "zone_id" {
+    default = "Z03935562DFKW1R55AYOI"
+}
